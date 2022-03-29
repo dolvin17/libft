d60850749aa9759721e742bba38267219104de99
ghuertas <ghuertas@student.42madrid.com>
2022-03-29 22:49:00 +0200

    corrected

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 09:07:38 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/03/24 16:52:17 by ghuertas         ###   ########.fr       */
+/*   Updated: 2022/03/28 06:38:09 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 /*PART I*/
 int		ft_atoi(const char *ptr);
 void	ft_bzero(void *s, size_t n);
-/*void	*ft_calloc(size_t count, size_t size);*/
+void	*ft_calloc(size_t count, size_t size);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -34,7 +34,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(const char *s, int c);
-/*char	*ft_strdup(const char *s1);*/
+char	*ft_strdup(const char *s1);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
