af5d2d3a8c7c7ddeb8a9fb76174b5f82cfbd4a18
ghuertas <ghuertas@student.42madrid.com>
2022-03-29 22:49:26 +0200

    corrected

---
+++ ft_strdup.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 06:37:50 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/03/29 15:03:50 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*d;
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	d = (char *)malloc(sizeof(char) * (s_len + 1));
+	if (d == NULL)
+		return (NULL);
+	ft_memcpy(d, s, s_len);
+	d[s_len] = '\0';
+	return (d);
+}
+/*
+int	main(void)
+{
+	const char	s[] = "Caramba";
+
+	printf("Original: %s\n", strdup);
+	printf("Mine: %s\n", ft_strdup);
+	system("leaks a.out");
+	return (0);
+}
+*/
