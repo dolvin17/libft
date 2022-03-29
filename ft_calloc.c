1762e7c1fe1a8737f08800f1e3ee9be5fd788574
ghuertas <ghuertas@student.42madrid.com>
2022-03-29 22:41:59 +0200

    corrected

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,37 @@
 /*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 11:38:19 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/03/24 17:14:18 by ghuertas         ###   ########.fr       */
+/*   Updated: 2022/03/29 22:38:51 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-void	*ft_calloc(size_t nitems, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
+	unsigned char	*p;
+	size_t			i;
 
-	if (!(ptr = malloc(nitems * size)))
-		return (NULL);
-	ft_bzero(ptr, nitems);
-	return (ptr);
+	p = malloc(count * size);
+	if (p != NULL)
+	{
+		i = 0;
+		while (i < count * size)
+			*(p + i++) = '\0';
+	}
+	return (p);
 }
+
+// int	main(void)
+// {
+// 	size_t count = 0;
+// 	size_t size = sizeof(int);
+// 	void *mine = ft_calloc(count, size);
+// 	void *original = calloc(count, size);
+// 	printf("Original: %s\n", original);
+// 	printf("Mine: %s\n", mine);
+// 	free(mine);
+// 	free(original);
+// 	system("leaks a.out");
+// 	return (0);
+// }
