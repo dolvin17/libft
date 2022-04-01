f0f41ab69e311569a1a88f49d617298ec02ddb39
ghuertas <ghuertas@student.42madrid.com>
2022-04-01 06:00:11 +0200

    last_update

---
+++ ft_split.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 15:06:13 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/04/01 05:17:18 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_words(char const *s, char c)
+{
+	int	nb;
+	int	i;
+
+	nb = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			nb++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (nb);
+}
+
+static char	*ft_place_word(char const *s, char c)
+{
+	char	*word;
+	int		i;
+	int		len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+			len++;
+	word = (char *)malloc(sizeof(char) * (len +1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static char	*ft_cpy_str(int i, char const *s, char c, char **res)
+{
+	res[i] = ft_place_word(s, c);
+	if (!res[i])
+	{
+		while (i > 0)
+		{
+			i--;
+			free(res[i]);
+		}
+		free(res);
+		return (NULL);
+	}
+	return (res[i]);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		nb_words;
+	int		i;
+	char	**res;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	nb_words = ft_count_words(s, c);
+	res = (char **)malloc(sizeof(char *) * (nb_words + 1));
+	if (!res)
+		return (NULL);
+	while (i < nb_words)
+	{
+		while (*s == c)
+			s++;
+		if (*s != '\0')
+			res[i] = ft_cpy_str(i, s, c, res);
+		while (*s && *s != c)
+			s++;
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
+/*
+int	main(void)
+{
+	int		i;
+	char	**tab;
+
+	i = 0;
+	tab = ft_split("marvin is a cat", 32);
+	while (i < 2)
+	{
+		printf("string: %d%s\n", i, tab[i]);
+		i++;
+	}
+	system ("leaks a.out");
+	return (0);
+}*/
