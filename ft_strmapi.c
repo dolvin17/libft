d880fa2424b0add52bab257d69dd36f216e30ce1
ghuertas <ghuertas@student.42madrid.com>
2022-04-01 06:01:18 +0200

    last_update

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 12:26:14 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/04/01 05:19:18 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	size_t		i;
+	char		*temp;
+
+	if (!s || !f)
+		return (NULL);
+	temp = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (i < (ft_strlen((char *)s)))
+	{
+		temp[i] = f(i, s[i]);
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
