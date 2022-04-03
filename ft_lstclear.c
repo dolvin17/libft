b968772bf0bf97baa97cbc20613affb23b14bd13
ghuertas <ghuertas@student.42madrid.com>
2022-04-04 00:28:40 +0200

    bonus_finalized

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 19:41:34 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/04/03 21:32:27 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*pos;
+
+	if (!lst)
+		return ;
+	while (*lst != NULL)
+	{
+		pos = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = pos;
+	}
+}
