30afc2170574cdea726f6fb84f47ddb9d8ac2884
ghuertas <ghuertas@student.42madrid.com>
2022-04-04 00:27:58 +0200

    bonus_finalized

---
+++ ft_lstadd_back.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 19:23:09 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/04/03 23:58:55 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*pos;
+
+	pos = ft_lstlast(*lst);
+	if (pos != NULL)
+		pos->next = new;
+	else
+		*lst = new;
+}
