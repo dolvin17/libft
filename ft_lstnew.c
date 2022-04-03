61fa8eda0fb4088f3efedfba3d3c0d01934759d2
ghuertas <ghuertas@student.42madrid.com>
2022-04-04 00:29:50 +0200

    bonus_finalized

---
+++ ft_lstnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 13:36:44 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/04/03 23:50:42 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
+
+/*int	main(void)
+{
+	t_list	*tamal;
+
+	tamal = ft_lstnew("HOLA SOY TAMALITO KAROLITO");
+	printf("%s", tamal->content);
+}
+*/
